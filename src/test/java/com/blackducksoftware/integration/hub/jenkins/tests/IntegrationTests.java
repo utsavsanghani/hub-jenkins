463923c053541f8daa6aa4b5ee8489072dc75a1f
jrichard <jrichard@blackducksoftware.com>
2014-06-25 14:26:05 -0400

    IJH-24 Added a class for Integration tests

---
+++ src/test/java/com/blackducksoftware/integration/hub/jenkins/tests/IntegrationTests.java
@@ -0,0 +1,48 @@
+package com.blackducksoftware.integration.hub.jenkins.tests;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class IntegrationTests {
+
+    private static final String PASSWORD_WRONG = "Assert.failurePassword";
+
+    private static final String USERNAME_NON_EXISTING = "Assert.failureUser";
+
+    private static String basePath;
+
+    private static Properties testProperties;
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @BeforeClass
+    public static void init() throws Exception {
+        basePath = IntegrationTests.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        basePath = basePath.substring(0, basePath.indexOf("/target"));
+        basePath = basePath + "/test-workspace";
+        testProperties = new Properties();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classLoader.getResourceAsStream("test.properties");
+        try {
+            testProperties.load(is);
+        } catch (IOException e) {
+            System.err.println("reading test.properties failed!");
+        }
+        // p.load(new FileReader(new File("test.properties")));
+        System.out.println(testProperties.getProperty("TEST_HUB_SERVER_URL"));
+        System.out.println(testProperties.getProperty("TEST_USERNAME"));
+        System.out.println(testProperties.getProperty("TEST_PASSWORD"));
+    }
+
+    @Test
+    public void blank() {
+
+    }
+}
